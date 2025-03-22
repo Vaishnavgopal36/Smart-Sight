2834de96cae41bfd2b619d4e31e003d15e4c3982
SreejithMVarma <sreejithmvarma@gmail.com>
2025-03-22 10:29:26 +0530

    refactor code

--- backend/server/app/settings.py
+++ backend/server/app/settings.py
@@ -1,9 +1,12 @@
 # backend/server/app/settings.py
+from dotenv import load_dotenv
 import os
 
+
+load_dotenv()  # Load environment variables from .env file
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # backend/server/app/ → backend/server/
 STATIC_DIR = os.path.join(BASE_DIR, "static")
 FAISS_INDEX_PATH = os.path.abspath(os.path.join(BASE_DIR, "..", "database", "flickr8k_faiss_index.faiss"))
 PATHS_FILE_PATH = os.path.abspath(os.path.join(BASE_DIR, "..", "database", "flickr8k_faiss_index.paths"))
 CAPTIONS_FILE_PATH = os.path.abspath(os.path.join(BASE_DIR, "..", "database", "captions.txt"))
-GEMINI_API_KEY = "************"  # Replace with your actual key
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
